cb939a91289ad637975e30f3400e0d961e832fbb
gmlstjq123 <gmlstjq123@naver.com>
2023-07-17 19:01:10 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("소스트리 실습!");
+        System.out.printf("gmlstjq123이 작성한 코드");
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
