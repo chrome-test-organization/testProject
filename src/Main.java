c5a3aaeed89a7de3b7ee915d1022916ac9594c48
gmlstjq123 <gmlstjq123@naver.com>
2023-07-17 18:32:35 +0900

    testProject-1 update app

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.sql.SQLOutput;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("소스트리 실습!");
         System.out.println("pull 실습!");
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
