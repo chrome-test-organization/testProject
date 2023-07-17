c7139a1d0d27ef3e773c4176bdbae0f2c30d9c3b
gmlstjq123 <gmlstjq123@naver.com>
2023-07-17 18:24:16 +0900

    pull 실습

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("소스트리 실습!");
+        System.out.println("pull 실습!");
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
